f2d4964fc134006c6fc812ce3ca0d0bf9257abf5
reversercoder <alainsergekossonou@gmail.com>
2020-03-28 16:00:29 +0100

    .env

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import * as  express from 'express'
 import * as bodyParser from 'body-parser'
- 
+import * as mysql from 'mysql' 
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
 class App {
+
+
   public app = express.Application
   public port: number
-
+  
   constructor(controllers, port){
+
     this.app = express()
-    this.port = port
- 
+    this.port = port || parseInt(process.env.PORT) || 5000
+
     this.initializeMiddlewares()
     this.initializeControllers(controllers)
+    this.dbConnection()
   }
  
   private initializeMiddlewares() {
@@ -39,6 +47,37 @@ class App {
     })
   }  
   
+  dbConnection(){
+    var host = process.env.DB_HOST || 'localhost'
+    var user = process.env.DB_USER || 'root'
+    var database = process.env.DATABASE || 'e-school'
+    var db_password = process.env.DB_PASS || ''
+    
+    console.log(
+      {
+        host: host,
+        user: user,
+        database: database,
+        db_password: db_password
+      }
+    )
+    const connection = mysql.createConnection({
+      host, user, db_password, database
+    })
+
+    const query = "SELECT 1+1"
+
+    connection.connect(err => {
+      if(err) throw err
+
+      connection.query(query, (err, res, field) => {
+        if(err) throw err
+        console.log(res)
+        
+      })
+    })
+
+  }
 }
 
 export default App
